93377d9ca46dafc994cbf8e80ddc032ae8b0972b
João Portela <joao.portela@gmail.com>
2011-10-28 23:24:54 +0100

    Schedule Plan ja cria schedules no servidor e na BD

--- src/pt/up/fe/cmov/PlannerActivity.java
+++ src/pt/up/fe/cmov/PlannerActivity.java
@@ -1,8 +1,13 @@
 package pt.up.fe.cmov;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
+import pt.up.fe.cmov.entities.Schedule;
+import pt.up.fe.cmov.entities.SchedulePlan;
+import pt.up.fe.cmov.operations.ScheduleOperations;
+import pt.up.fe.cmov.operations.SchedulePlanOperations;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
@@ -18,10 +23,15 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PlannerActivity extends Activity {
-		
+	
+	public static final String PLANNER_DOCTOR_ID = "plannerDoctorId";
+	
 	private OnClickListener newRowListener =    
 		new OnClickListener() {
 
@@ -65,9 +75,11 @@ public class PlannerActivity extends Activity {
 
 		@Override
 		public void onClick(View v) {
-			if (mYear == 0)	//ERROR
+			if (mYear == 0)	{
+				Toast.makeText(PlannerActivity.this, "Please add a Start Date", Toast.LENGTH_LONG);
 				return;
-			Date date = new Date(mYear, mMonth, mDay);
+			}
+			Date date = new Date(mYear-1900, mMonth, mDay);
 			
 			Log.i("DATE", "" + date.toString());
 			
@@ -75,15 +87,52 @@ public class PlannerActivity extends Activity {
 			int START_HOUR_POS = 1;
 			int END_HOUR_POS = 3;
 
-			LinearLayout plannerRows = (LinearLayout) findViewById(R.id.plannerRows);
+			TableLayout plannerRows = (TableLayout) findViewById(R.id.plannerRows);
+			
+			SchedulePlan plan = new SchedulePlan(-1, mDoctorId, date);
+			
+			ArrayList<Schedule> schedules = new ArrayList<Schedule>();
 
 			for (int i = 0; i < plannerRows.getChildCount(); i++) {
-				LinearLayout row = (LinearLayout) plannerRows.getChildAt(i);
+				TableRow row = (TableRow) plannerRows.getChildAt(i);
 				
 				Spinner weekdaySpinner = (Spinner)row.getChildAt(WEEKDAY_POS);
 				int weekday = (int) weekdaySpinner.getSelectedItemId();
-				Log.i("WEEK", "" + weekday);
+				Log.i("Planner", "Week: " + weekday);
 				
+				Spinner startHourSpinner = (Spinner)row.getChildAt(START_HOUR_POS);
+				int startHour = (int) startHourSpinner.getSelectedItemId();
+				Log.i("Planner", "Start Hour: " + startHour);
+				
+				Spinner endHourSpinner = (Spinner)row.getChildAt(END_HOUR_POS);
+				int endHour = (int) endHourSpinner.getSelectedItemId();
+				Log.i("Planner", "End Hour: " + endHour);
+				
+				if (startHour == endHour) {
+					Toast.makeText(PlannerActivity.this, "Start and End hour cannot be the same", Toast.LENGTH_LONG);
+					return;
+				}
+				
+				Date startDate = calculateDate(date, weekday, startHour);
+				
+				Log.i("Planner", "Start date: " + startDate.toString());
+				if (startHour > endHour)
+					endHour += 48;	//next day
+
+				Date endDate = calculateDate(date, weekday, endHour);
+
+				Log.i("Planner", "End date: " + endDate.toString());
+				
+				Schedule schedule = new Schedule(-1, startDate, endDate);
+				schedules.add(schedule);
+			}
+			
+			int planId = SchedulePlanOperations.createSchedulePlan(PlannerActivity.this, plan, true);
+			
+			for (int i=0; i < schedules.size(); i++) {
+				Schedule schedule = schedules.get(i);
+				schedule.setSchedulePlanId(planId);
+				ScheduleOperations.createSchedule(PlannerActivity.this, schedule, true);
 			}
 		}
 
@@ -92,6 +141,8 @@ public class PlannerActivity extends Activity {
 
     public static final int DATE_DIALOG_ID = 0;
 
+    private int mDoctorId = 0;
+    
     private int mYear = 0, mMonth = 0, mDay = 0;
     
 	private DatePickerDialog.OnDateSetListener mDateSetListener =
@@ -104,7 +155,7 @@ public class PlannerActivity extends Activity {
           mDay = dayOfMonth;
           
           Button b = (Button) findViewById(R.id.datePickerButton);
-          String text = "" + mDay + "/" + mMonth+1 + "/" + mYear;
+          String text = "" + mDay + "/" + (mMonth+1) + "/" + mYear;
           b.setText(text);
       }
 
@@ -114,6 +165,13 @@ public class PlannerActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.planner);
+        
+        mDoctorId = getIntent().getIntExtra(PLANNER_DOCTOR_ID, 0);
+        
+        if (mDoctorId == 0)
+        {
+        	Toast.makeText(this, "No doctor associated with this planner", Toast.LENGTH_LONG);
+        }
         
         Button b = (Button) findViewById(R.id.datePickerButton);
         b.setOnClickListener(callDatePickerListener);
@@ -128,13 +186,13 @@ public class PlannerActivity extends Activity {
 	}
 	
 	public void addPlannerRow() {
-		LinearLayout plannerRow = new LinearLayout(this);
+		TableRow plannerRow = new TableRow(this);
 		
 		plannerRow.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-		plannerRow.setPadding(0, 10, 0, 10);
+		//plannerRow.setPadding(0, 10, 0, 10);
 		
 		Spinner weekdaySpinner = new Spinner(this);
-		weekdaySpinner.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+		weekdaySpinner.setLayoutParams(new TableRow.LayoutParams(0));
 		weekdaySpinner.setPromptId(R.string.weekdays_prompt);
 	    weekdaySpinner.setBackgroundDrawable(getResources().getDrawable(android.R.drawable.btn_default));
 
@@ -144,7 +202,7 @@ public class PlannerActivity extends Activity {
 	    weekdaySpinner.setAdapter(adapter);
 	    
 	    Spinner startHourSpinner = new Spinner(this);
-	    startHourSpinner.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+	    startHourSpinner.setLayoutParams(new TableRow.LayoutParams(1));
 	    startHourSpinner.setPromptId(R.string.hours_prompt);
 	    startHourSpinner.setBackgroundDrawable(getResources().getDrawable(android.R.drawable.btn_default));
 
@@ -155,26 +213,26 @@ public class PlannerActivity extends Activity {
 	    startHourSpinner.setAdapter(hourAdapter);
 	    
 	    TextView toLabel = new TextView(this);
-	    toLabel.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.FILL_PARENT));
+	    toLabel.setLayoutParams(new TableRow.LayoutParams(2));
 	    toLabel.setText(R.string.plannerToLabel);
 	    toLabel.setGravity(Gravity.CENTER_VERTICAL);
 	    toLabel.setPadding(10, 0, 10, 0);
 	    toLabel.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 16);
 	    
 	    Spinner endHourSpinner = new Spinner(this);
-	    endHourSpinner.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+	    endHourSpinner.setLayoutParams(new TableRow.LayoutParams(3));
 	    endHourSpinner.setPromptId(R.string.hours_prompt);
 	    endHourSpinner.setBackgroundDrawable(getResources().getDrawable(android.R.drawable.btn_default));
 	    
 	    endHourSpinner.setAdapter(hourAdapter);
 	    
 	    Button addRowButton = new Button(this);
-	    addRowButton.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+	    addRowButton.setLayoutParams(new TableRow.LayoutParams(4));
 	    addRowButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.addbutton));
 	    addRowButton.setOnClickListener(newRowListener);
 	    
 	    Button eraseRowButton = new Button(this);
-	    eraseRowButton.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+	    eraseRowButton.setLayoutParams(new TableRow.LayoutParams(5));
 	    eraseRowButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.deletebutton));
 	    eraseRowButton.setOnClickListener(eraseRowListener);
 
@@ -185,7 +243,7 @@ public class PlannerActivity extends Activity {
 	    plannerRow.addView(addRowButton); 
 	    plannerRow.addView(eraseRowButton);
 	    
-        LinearLayout plannerRows = (LinearLayout) findViewById(R.id.plannerRows);
+        TableLayout plannerRows = (TableLayout) findViewById(R.id.plannerRows);
         plannerRows.addView(plannerRow);
 	}
 	
@@ -198,6 +256,21 @@ public class PlannerActivity extends Activity {
 	                    mYear, mMonth, mDay);
 	    }
 	    return null;
+	}
+	
+	public static Date calculateDate(Date startDate, int weekday, int hour) {
+		Calendar c1 = Calendar.getInstance();
+		c1.setTime(startDate);
+		int startWeekday = c1.get(Calendar.DAY_OF_WEEK);
+		weekday = (weekday == 6) ? 0 : weekday+1;	//adjust indexes  
+		
+		int addWeek = weekday - startWeekday + 1;
+		if (addWeek < 0)
+			addWeek += 7;
+		
+		c1.add(Calendar.DAY_OF_WEEK, addWeek);
+		c1.add(Calendar.MINUTE, hour*30);
+		return c1.getTime();
 	}
 	
 }
